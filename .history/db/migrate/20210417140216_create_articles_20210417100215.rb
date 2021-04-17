3130884906f23c46e552054658ad46f1e11010ed
Randy Steele <steelerx155@Gmail.com>
2021-04-17 12:09:18 -0400

    added css

--- .history/db/migrate/20210417140216_create_articles_20210417100215.rb
+++ .history/db/migrate/20210417140216_create_articles_20210417100215.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.string :content
+      t.string :title,
+      t.string :content,
 
       t.timestamps
     end
